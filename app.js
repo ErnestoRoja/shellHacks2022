e16431936515e3f53a8c7736562c82f96c9e493a
ErnestoRoja <ernestoroja@ufl.edu>
2022-09-10 13:35:50 -0400

    finished login screen

---
+++ app.js
@@ -0,0 +1,15 @@
+var sign_in = document.getElementById("sign-in");
+var reg = document.getElementById("register");
+var slider = document.getElementById("button");
+
+function register(){
+    sign_in.style.left = "-350px";
+    reg.style.left = "92px";
+    slider.style.left = "110px";
+}
+
+function login(){
+    sign_in.style.left = "90px";
+    reg.style.left = "550px";
+    slider.style.left = "0px";
+}
